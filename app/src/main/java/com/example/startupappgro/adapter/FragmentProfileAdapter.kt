6b47df723f9222f50c447bccdf50a6ffcac687c0
MJackson22-bit <jm5444859@gmail.com>
2022-05-09 22:56:32 -0600

    Add Fragment Publication and Fragment Edit Profile

---
+++ app/src/main/java/com/example/startupappgro/adapter/FragmentProfileAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.startupappgro.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.startupappgro.plant.fragment.AllPlantFragment
+import com.example.startupappgro.plant.fragment.ExteriorsFragment
+import com.example.startupappgro.plant.fragment.GardenFragment
+import com.example.startupappgro.plant.fragment.InteriorsFragment
+
+class FragmentProfileAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) : FragmentStateAdapter(fragmentManager, lifecycle) {
+    override fun getItemCount(): Int {
+        return 2
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return when(position){
+            0 -> {
+                AllPlantFragment()
+            }
+            1 -> {
+                GardenFragment()
+            }
+            2 -> {
+                InteriorsFragment()
+            }
+            3 -> {
+                ExteriorsFragment()
+            }
+            else -> {
+                Fragment()
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/startupappgro/plant/fragment/PublicationsFragment.kt
+++ app/src/main/java/com/example/startupappgro/plant/fragment/PublicationsFragment.kt
@@ -5,13 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import com.example.startupappgro.R
-import com.example.startupappgro.adapter.FragmentMascotaAdapter
-import com.example.startupappgro.databinding.FragmentPerfilBinding
-import com.example.startupappgro.databinding.FragmentScannerBinding
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.tabs.TabLayoutMediator
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,15 +14,13 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [PerfilFragment.newInstance] factory method to
+ * Use the [PublicationsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class PerfilFragment : Fragment() {
+class PublicationsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-    private var _binding: FragmentPerfilBinding? = null
-    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,32 +34,8 @@ class PerfilFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentPerfilBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding.pagerPerfil.isSaveEnabled = false
-        initPager()
-    }
-
-    private fun initPager() {
-        val adapter = FragmentMascotaAdapter(fragmentManager = this.childFragmentManager, lifecycle = lifecycle)
-        binding.pagerPerfil.adapter = adapter
-        TabLayoutMediator(binding.tabLayoutPerfil, binding.pagerPerfil
-        ) { tab, position ->
-            when(position){
-                0 -> {
-                    tab.text = "Publicaciones"
-                }
-                1 -> {
-                    tab.text = "Editar"
-                }
-            }
-        }.attach()
-
-
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_publications, container, false)
     }
 
     companion object {
@@ -77,12 +45,12 @@ class PerfilFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment PerfilFragment.
+         * @return A new instance of fragment PublicationsFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            PerfilFragment().apply {
+            PublicationsFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
